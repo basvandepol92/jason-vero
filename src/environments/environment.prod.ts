a4ee96281537023299ca3044c9f5d092037a51cb
Bas van de Pol <bas.van.de.pol@rabobank.nl>
2025-12-24 13:43:13 +0100

    Connect to Directus on Railway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8055',
+  apiUrl: 'https://directus-production-f307.up.railway.app',
   oneSignalAppId: '55ea6da9-20ef-4b62-97e2-84fc7f4ff3f8'
 };
